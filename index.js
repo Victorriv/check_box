44ad1fa86b9bae1c80008c02d6624eb765d316ab
Victorriv <Victorrivera714@yahoo.com>
2024-12-05 11:32:11 -0800

    mySubmit const

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ const myCheckBox = document.getElementById("myCheckBox");
 const visaBtn = document.getElementById("visaBtn");
 const masterCardBtn = document.getElementById("masterCardBtn");
 const payPalBtn = document.getElementById("payPalBtn");
-
+const mySubmit = document.getElementById("mySubmit");
