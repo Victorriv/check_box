86afa29d5d1b958bbe926af14c6b3378a7e04bc7
Victorriv <Victorrivera714@yahoo.com>
2024-12-05 11:27:01 -0800

    setting up constant for checkBox

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // . checked = property that determines the checked state of an html checkbox or radio button element. 
 
+// get the elements by id and store them in constants
+// elements from html.
+
+
+const myCheckBox = document.getElementById("myCheckBox")
+
