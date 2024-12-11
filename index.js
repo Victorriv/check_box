f9c2401609a56d39a1ce5cc5783029abad75981b
Victorriv <Victorrivera714@yahoo.com>
2024-12-10 18:18:12 -0800

    creating a subresult

--- index.js
+++ index.js
@@ -9,3 +9,5 @@ const visaBtn = document.getElementById("visaBtn");
 const masterCardBtn = document.getElementById("masterCardBtn");
 const payPalBtn = document.getElementById("payPalBtn");
 const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+
